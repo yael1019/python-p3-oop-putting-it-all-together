25a56005ac3aee5b84c73036b9b3a68199277ef6
Yael Harosh <yaelharosh01@gmail.com>
2023-04-04 14:57:19 -0400

    pass 4 tests

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+    def page_count(self, num):
+        if(isinstance(num, int)):
+            self.page_num = num
+    # num = property(page_count)
