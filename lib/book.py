9acea4272f73f5d7f12f1aa70e2e94c2006cb25a
Yael Harosh <yaelharosh01@gmail.com>
2023-04-05 16:38:58 -0400

    pass tests

--- lib/book.py
+++ lib/book.py
@@ -3,7 +3,7 @@
 class Book:
     def __init__(self, title, page_count = 0):
         self.title = title
-        self._page_count = page_count
+        self.page_count = page_count
     
     def get_page_count(self):
         return self._page_count
@@ -16,5 +16,7 @@ class Book:
     
     page_count = property(get_page_count, set_page_count)
 
-    def turn_page():
+    def turn_page(self):
         print("Flipping the page...wow, you read fast!")
+
+# book = Book('yay', '120')
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size = 0):
+        self.brand = brand
+        self.size = size
+    
+    def get_size(self):
+        return self._brand
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._brand = size 
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def cobble(self, condition = 'Used'):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
